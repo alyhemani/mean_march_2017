8896c4095cae2cde3007b4a86f3b0180f78338b2
Zach Page <zap.freelance@gmail.com>
2017-03-15 15:06:31 -0500

    cleaned up folders

---
+++ Zach_Page/Angular/secondAngular/server.js
@@ -0,0 +1,11 @@
+var express = require('express')
+
+var app = express();
+
+app.use(express.static(__dirname, 'static'));
+app.use(express.static(__dirname, 'bower_components'));
+
+
+app.listen(8000, function(){
+  console.log("Listening on 8000...");
+});
